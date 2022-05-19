823bcc12b14831cdd86ec0b96204beb969f460ec
01fe21mve005 <105493686+01fe21mve005@users.noreply.github.com>
2022-05-19 15:33:08 +0530

    Add files via upload

---
+++ Sequence Detector/SD_moore.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:04:29 04/08/2022 
+// Design Name: 
+// Module Name:    SD_moore 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module SD_moore;
+ reg sequence_in;
+ reg clock;
+ reg reset;
+ wire detector_out;
+
+ SD uut (
+  .sequence_in(sequence_in), 
+  .clock(clock), 
+  .reset(reset), 
+  .detector_out(detector_out)
+ );
+ initial begin
+ clock = 0;
+ forever #5 clock = ~clock;
+ end 
+ initial begin
+ 
+  sequence_in = 0;
+  reset = 1;
+  #30;
+  reset = 0;
+  #40;
+  sequence_in = 1;
+  #10;
+  sequence_in = 0;
+  #10;
+  sequence_in = 1; 
+  #20;
+  sequence_in = 0; 
+  #20;
+  sequence_in = 1; 
+  #20;
+  sequence_in = 0;  
+  
+end
+initial  begin
+   $dumpfile ("dump.vcd"); 
+   $dumpvars; 
+end 
+initial  begin
+ $monitor("%d,\t%b,\t%b,\t%b,\t%d",$time,sequence_in,clock,reset,detector_out); 
+  end 
+  initial 
+  #1000  $finish;
+ endmodule
